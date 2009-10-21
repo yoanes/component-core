86b6faf5f062656a25b8d8158c6f4d01a677ee3f
w12495 <w12495>
2009-10-21 22:18:16 +0000

    Added dummy placeholder device-variant JavaScript files for apple and maste due to the current build scripts (based on the prototype) having flaws: 1. The build scripts will fail if the device-variant source directory does not exist. This in turn can easily happen if the directory exists in CVS but it is empty, leading to the directory not being checked out under typical CVS -prune behaviour. 2. The current component strucure will not produce any bundles if there is no device variant source, even if there is device invariant source.

---
+++ src/javascript/device-variant/master/master.js
@@ -0,0 +1,6 @@
+// TODO: empty placeholder JavaScript file. Current build scripts (based on the prototype) has flaws:
+// 1. The build scripts will fail if the device-variant source directory does not exist. This in turn can easily
+//    happen if the directory exists in CVS but it is empty, leading to the directory not being checked out 
+//    under typical CVS -prune behaviour.
+// 2. The current component strucure will not produce any bundles if there is no device variant source,
+//    even if there is device invariant source.
