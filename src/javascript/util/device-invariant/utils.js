6fe67f38af8c02d2400b8fdbeb153e9ea8f4d489
w12495 <w12495>
2010-01-13 22:57:14 +0000

    Updated version to 1.0.8: Added new util subcomponent to serve up Yoanes' utils.js. Widened the list of devices that the core and logging MSCR files apply to (still not the "real" list - just for current adhoc testing).

---
+++ src/javascript/util/device-invariant/utils.js
@@ -0,0 +1,103 @@
+/*******************************************************************************************
+ * SENSIS MOBILES COMMON UTILITIES
+ * 
+ * Version 1.0
+ * 
+ * Provides common Javscript functions that can be used across mobile applications.
+ * 
+ * Note: These functions have intentionally been placed within a single Class 
+ * to improve performance on mobile devices with limited memory. 
+ * 
+ * Dependencies:
+ * - mootools.js
+ * 
+ * Change Log:
+ * 	1.0		6/05/2009		Initial version released.		Tony Filipe, Yoanes Koesno
+ *   
+ *******************************************************************************************/
+
+var Utilities = new Class({
+	
+	version: '1.0',
+	
+	/**
+	 * Returns the JSessionId appended to the current window URL, if any.
+	 */	
+	getJSessionId: function(){
+		var path = new String(window.location);
+		var start = path.indexOf(';');
+		var sessionId = "";
+		
+		/* try to get from the location's URL */
+		if (start > -1) {
+			var end = path.indexOf("?");
+			if (end > -1) {
+				sessionId = path.substring(start, end);
+			} else {
+				sessionId = path.substring(start);
+			}
+		}
+		
+		/* if nothing found in the current location, look from the cookie */
+		else {
+			var fromCookie = Cookie.read('JSESSIONID');
+			if($defined(fromCookie)) sessionId = ';jsessionid=' + fromCookie;
+		}
+		
+		return sessionId;
+	},
+
+	/**
+	 * Appends the JSessionId (if one exists) to the given URL, maintaining any get parameters.
+	 * 
+	 * @param url The URL to append the session id to
+	 * @return The URL with session id appended (if one exists)
+	 */
+	maintainSession: function(url) {
+		var sessid = this.getJSessionId();
+		if (sessid != '') {
+			var urlPrts = url.split("?");
+			url = urlPrts[0] + sessid;
+			if (urlPrts.length > 1) {
+				url+= "?" + urlPrts[1];
+			}
+		}
+		return url;
+	}
+});
+
+var ReportingUtilities = new Class({
+	Extends: Utilities,
+	
+	to: function(lUrl, optionalParams, optionalHeaders) {
+		if($defined(lUrl) && lUrl.length > 0){
+			
+			var ajaxReport = new Request({
+				method: 'get',
+				url: this.maintainSession(lUrl),
+				onComplete: function() {}
+			});
+			
+			var extraParams = null;
+			
+			if($defined(optionalParams)){ 
+				extraParams = new String();
+				var first = true;
+				for(var param in optionalParams) {
+					if(!first) extraParams += '&';
+					else first = false;
+					
+					extraParams += param + '=' + optionalParams[param];
+				}
+			}
+			
+			if($defined(optionalHeaders)) 
+				for(var attribute in optionalHeaders)
+					ajaxReport.setHeader(attribute, optionalHeaders[attribute]);
+					
+			ajaxReport.send(extraParams);
+		}
+	}
+});
+
+var Reporting = new ReportingUtilities();
