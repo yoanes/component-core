91d0fb537f538f1b85f633a4d6e4bebdede3a7c8
w89477 <w89477>
2010-04-07 00:33:45 +0000

    commetns

--- src/javascript/deviceport/device-variant/iphone/deviceport.js
+++ src/javascript/deviceport/device-variant/iphone/deviceport.js
@@ -1,6 +1,8 @@
+/* create the meta tag for viewport */
 var meta = document.createElement('meta');
 meta.setAttribute('name', 'viewport');
 meta.setAttribute('content', 'width=device-width; initial-scale=1.0; maximum-scale=1.0');
 document.getElementsByTagName('head')[0].appendChild(meta);
 
+/* hide the address bar */
 window.addEventListener('load', function() { window.scroll(0, 1); }, true);
