7bb9f1f2d53c01153c7d3e18c4fc85646395a7e0
w89477 <w89477>
2010-04-07 01:01:41 +0000

    add another meta to remove the phone link

--- src/javascript/deviceport/device-variant/apple/deviceport.js
+++ src/javascript/deviceport/device-variant/apple/deviceport.js
@@ -1,8 +1,14 @@
 /* create the meta tag for viewport */
-var meta = document.createElement('meta');
-meta.setAttribute('name', 'viewport');
-meta.setAttribute('content', 'width=device-width; initial-scale=1.0; maximum-scale=1.0');
-document.getElementsByTagName('head')[0].appendChild(meta);
+var metaViewport = document.createElement('meta');
+metaViewport.setAttribute('name', 'viewport');
+metaViewport.setAttribute('content', 'width=device-width; initial-scale=1.0; maximum-scale=1.0');
+document.getElementsByTagName('head')[0].appendChild(metaViewport);
+
+/* create the meta tag to kill the phone link detection */
+var metaFormat = document.createElement('meta');
+metaFormat.setAttribute('name', 'format-detection');
+metaFormat.setAttribute('content', 'telephone=no');
+document.getElementsByTagName('head')[0].appendChild(metaFormat);
 
 /* hide the address bar */
 window.addEventListener('load', function() { window.scroll(0, 1); }, true);
