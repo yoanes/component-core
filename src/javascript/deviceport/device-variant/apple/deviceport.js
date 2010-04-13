dea4782e039a78a2d622ac20fae40fe06e408b1a
w89477 <w89477>
2010-04-13 07:52:16 +0000

    assign device width = 320

--- src/javascript/deviceport/device-variant/apple/deviceport.js
+++ src/javascript/deviceport/device-variant/apple/deviceport.js
@@ -1,7 +1,7 @@
 /* create the meta tag for viewport */
 var metaViewport = document.createElement('meta');
 metaViewport.setAttribute('name', 'viewport');
-metaViewport.setAttribute('content', 'width=device-width; initial-scale=1.0; maximum-scale=1.0');
+metaViewport.setAttribute('content', 'width=320; initial-scale=1.0; maximum-scale=1.0');
 document.getElementsByTagName('head')[0].appendChild(metaViewport);
 
 /* create the meta tag to kill the phone link detection */
