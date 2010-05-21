f58961ae7f1f11c556b715ec72cfa4fe9f7ef765
w89477 <w89477>
2010-05-21 00:43:01 +0000

    htcTouchHD 2 variant. meta name = mobileoptimized

---
+++ src/javascript/deviceport/device-variant/htcTouchHD2/deviceport.js
@@ -0,0 +1,4 @@
+var meta = document.createElement('meta');
+meta.setAttribute('name', 'mobileoptimized');
+meta.setAttribute('content', 'width=470');
+document.getElementsByTagName('head')[0].appendChild(meta);
