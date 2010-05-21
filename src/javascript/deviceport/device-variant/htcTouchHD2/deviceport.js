b9cf9d9ca33e9433d29fb359438afa5b61eb4991
w89477 <w89477>
2010-05-21 03:57:23 +0000

    use viewport instead and width=470 and initial-scale=1.0

--- src/javascript/deviceport/device-variant/htcTouchHD2/deviceport.js
+++ src/javascript/deviceport/device-variant/htcTouchHD2/deviceport.js
@@ -1,4 +1,4 @@
 var meta = document.createElement('meta');
-meta.setAttribute('name', 'mobileoptimized');
-meta.setAttribute('content', 'width=470');
+meta.setAttribute('name', 'viewport');
+meta.setAttribute('content', 'width=470, initial-scale=1.0');
 document.getElementsByTagName('head')[0].appendChild(meta);
